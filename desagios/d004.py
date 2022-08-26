d643eef9ce0eef701e4d5fb9e5567f37a959fb6a
UenoSilva <uenosouzasilva@gmail.com>
2022-08-25 21:50:40 -0300

    desafio 4

---
+++ desagios/d004.py
@@ -0,0 +1,6 @@
+var = input('Digite algo: ')
+print(type(var))
+print('É um número alfanúmerico?', var.isalnum())
+print('É um digito?', var.isdigit())
+print('É um decimal?', var.isdecimal())
+print('É um número', var.isnumeric())
