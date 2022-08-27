4ccea87c2724a85625ef11a8ec57a88e50970adf
UenoSilva <uenosouzasilva@gmail.com>
2022-08-26 21:18:46 -0300

    desafio 6

---
+++ desafios/d006.py
@@ -0,0 +1,4 @@
+valor = int(input('Digite um valor: '))
+print('O dobro de {} é {}'.format(valor, valor*2))
+print('O triplo de {} é {}'.format(valor, valor*3))
+print('A raiz quadrada de {} é {}'.format(valor, pow(valor, 1/2)))
