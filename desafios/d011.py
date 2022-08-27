bfff6d10be0cca832623e0b46f94077ff13edc1d
UenoSilva <uenosouzasilva@gmail.com>
2022-08-26 21:38:41 -0300

    desafio 11

---
+++ desafios/d011.py
@@ -0,0 +1,5 @@
+altura = float(input('Digite a altura da parede: '))
+largura = float(input(('Digite a largura da parede: ')))
+area = largura*altura
+print('A área da parede é {}'.format(area))
+print('Serão necessários {} litros de tinta'.format(area/2))
