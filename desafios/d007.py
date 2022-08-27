ce4fa07f7aa1f57cdf63b9cf252c5e339258173a
UenoSilva <uenosouzasilva@gmail.com>
2022-08-26 21:21:34 -0300

    desafio 7

---
+++ desafios/d007.py
@@ -0,0 +1,5 @@
+nota1 = float(input('Digite a nota 1: '))
+nota2 = float(input('Digite a nota 2: '))
+
+print('Primeira nota: {} \nSegunda nota: {}'.format(nota1, nota2))
+print('A média é {}'.format((nota1+nota2)/2))
