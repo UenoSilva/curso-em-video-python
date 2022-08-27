7eb2e0f4293aa4ec4908c3b049bd53597f357e38
UenoSilva <uenosouzasilva@gmail.com>
2022-08-26 21:45:37 -0300

    desafio 12

---
+++ desafios/d012.py
@@ -0,0 +1,3 @@
+precoProduto = float(input('Valor do produto: '))
+precoComDesconto = precoProduto*((100-5)/100)
+print('Novo preço do produto é {}, desconto de {}'.format(precoComDesconto, precoProduto*(5/100)))
