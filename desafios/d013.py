8f6f90435b69cebcdffb03b0daaca54eecc8afc3
UenoSilva <uenosouzasilva@gmail.com>
2022-08-26 22:25:00 -0300

    desafio 13

---
+++ desafios/d013.py
@@ -0,0 +1,4 @@
+salarioAtual = float(input('Digite o salário atual: '));
+novoSalario = salarioAtual * ((100+15)/100)
+print('O novo salário será de {} reais'.format(novoSalario))
+print('reajuste de {} reais'.format(salarioAtual*(15/100)))
