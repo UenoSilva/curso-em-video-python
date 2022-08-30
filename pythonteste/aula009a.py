8b2d4d1396fcbb5ec9f4c1c4ca17c6239f133a54
UenoSilva <uenosouzasilva@gmail.com>
2022-08-30 20:37:16 -0300

    aula 9

---
+++ pythonteste/aula009a.py
@@ -0,0 +1,13 @@
+frase = 'Curso em Video Python'
+print(frase[:14])
+print(frase[::3])
+
+print('''Erosão Antrópica: Esse tipo é quando a erosão 
+é causada devido a ação humana. Ou seja, não ocorre 
+de forma natural. Sendo assim, pode ser através de 
+uma reação química. Outros casos, por exemplo, 
+são o desmatamento, impermeabilização do solo e 
+em áreas de plantação. Esses são uns dos principais 
+fatores que causam a erosão antes da hora.''')
+
+
---
+++ pythonteste/aula009b.py
@@ -0,0 +1,15 @@
+frase = 'Curso em Video Python'
+print(frase.strip(' '))
+print(frase.replace('Python', 'Android'))
+
+print(frase)
+frase = frase.replace('Python', 'Android')
+print(frase)
+
+print(frase.lower())
+
+print(frase.split())
+
+print(frase.count(' '))
+
+
