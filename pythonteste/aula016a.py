c4032ffa2060179d2c0947604197e2db9b18dd41
UenoSilva <uenosouzasilva@gmail.com>
2022-09-16 18:56:18 -0300

    aula 16

---
+++ pythonteste/aula016a.py
@@ -0,0 +1,12 @@
+lanche = ('hambúrguer', 'suco', 'pizza', 'pudim')
+
+#for i in lanche:
+#    print(f'Eu vou comer {i}')
+
+#for cont in range(0, len(lanche)):
+#    print(lanche[cont])
+
+for pos, comida in enumerate(lanche):
+    print(f'Vou comer {comida} na posição {pos}')
+
+print('Comi pra caramba!')
