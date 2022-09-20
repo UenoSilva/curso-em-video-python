6aa404200481b05c8a969bca7ac71b2459d20c67
UenoSilva <uenosouzasilva@gmail.com>
2022-09-20 03:22:43 -0300

    aula 18

---
+++ pythonteste/aula018c.py
@@ -0,0 +1,13 @@
+galera = list()
+dado = list()
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+
+for p in galera:
+    if p[1] >= 21:
+        print(f'{p[0]} é maior de idade.')
+    else:
+        print(f'{p[0]} é menor de idade.')
