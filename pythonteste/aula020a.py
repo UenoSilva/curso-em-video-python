3ed690acc36721a9c31e4b2fe66c7fcc4348e111
UenoSilva <uenosouzasilva@gmail.com>
2022-10-01 12:09:01 -0300

    aula 20

---
+++ pythonteste/aula020a.py
@@ -0,0 +1,12 @@
+def soma(a, b):
+    print('-'*30)
+    print(f'A = {a} e B = {b}')
+    soma = a + b
+    print(f'A soma de A + B = {soma}')
+    print('-'*30)
+
+
+#Programa Principal
+soma(9, 8)
+soma(b=5, a=87)
+soma(1, 3)
---
+++ pythonteste/aula020b.py
@@ -0,0 +1,6 @@
+def num(*num):
+    print(f'{sorted(num)}')
+
+
+num(2, 7, 8, 5, 3, 4)
+num(3, 4, 5, 6, 7, 8, 9, 10)
